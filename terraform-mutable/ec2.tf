98a90f7bdf00d92727107dd924fe71c1f2a0453b
Siny <sinydaniel93@gmail.com>
2022-11-08 16:43:23 -0500

    jenkins file in frontend

--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_spot_instance_request" "ec2-spot" {
   count = var.INSTANCE_COUNT
-  ami           = data.aws_ami.ami.id
+  ami = data.aws_ami.ami.id
 
   instance_type = var.INSTANCE_TYPE
 
