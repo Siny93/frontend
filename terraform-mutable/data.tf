b505e7a17110314db843d32f4eb5c8bb9e4d909a
Siny <sinydaniel93@gmail.com>
2022-11-11 17:42:09 -0500

    ec2

--- terraform-mutable/data.tf
+++ terraform-mutable/data.tf
@@ -3,3 +3,13 @@ data "aws_ami" "ami" {
   name_regex = "base-with-ansible"
   owners = ["self"]
 }
+
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-9366"
+    key    = "vpc/${var.ENV}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
