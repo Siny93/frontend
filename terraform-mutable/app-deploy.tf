a6d07f7dcd9041ee86b6cfbadb9fbbb3e7bfa7e7
Siny <sinydaniel93@gmail.com>
2022-12-06 18:39:45 -0500

    app deploy

--- terraform-mutable/app-deploy.tf
+++ terraform-mutable/app-deploy.tf
@@ -9,7 +9,7 @@ resource "null_resource" "app-deploy" {
       host     = aws_spot_instance_request.ec2-spot.*.private_ip[count.index]
     }
     inline = [
-      "ansible-pull -U https://github.com/Siny93/ANSIBLE1.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${APP_VERSION}"
+      "ansible-pull -U https://github.com/Siny93/ANSIBLE1.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${APP_VERSION} -e NEXUS_USERNAME=${jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["NEXUS_USERNAME"]} -e NEXUS_PASSWORD=${jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["NEXUS_PASSWORD"]}"
     ]
   }
 }
