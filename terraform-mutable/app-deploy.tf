83614e24fb6334befd816a1d7fe3a1e82bb7671c
Siny <sinydaniel93@gmail.com>
2022-12-06 17:45:49 -0500

    provisioner

--- terraform-mutable/app-deploy.tf
+++ terraform-mutable/app-deploy.tf
@@ -8,8 +8,8 @@ resource "null_resource" "app-deploy" {
       password = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_PASSWORD"]
       host     = aws_spot_instance_request.ec2-spot.*.private_ip[count.index]
     }
-    inline = [
-      "ansible-pull -U https://github.com/Siny93/ANSIBLE1.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV}"
-    ]
+//    inline = [
+//      "ansible-pull -U https://github.com/Siny93/ANSIBLE1.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV}"
+//    ]
   }
 }
