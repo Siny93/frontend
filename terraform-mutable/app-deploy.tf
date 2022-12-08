724dc31f0e59847d1542fb321ef2b8a92296e1dc
Siny <sinydaniel93@gmail.com>
2022-12-08 17:47:32 -0500

    app deploy

--- terraform-mutable/app-deploy.tf
+++ terraform-mutable/app-deploy.tf
@@ -3,13 +3,13 @@ resource "null_resource" "app-deploy" {
   provisioner "remote-exec" {
 
     connection {
-      type     = "ssh"
-      user     = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USERNAME"]
+      type = "ssh"
+      user = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_USERNAME"]
       password = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["SSH_PASSWORD"]
-      host     = aws_spot_instance_request.ec2-spot.*.private_ip[count.index]
+      host = aws_spot_instance_request.ec2-spot.*.private_ip[count.index]
     }
     inline = [
       "ansible-pull -U https://github.com/Siny93/ANSIBLE1.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${APP_VERSION} -e NEXUS_USERNAME=${nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["NEXUS_USERNAME"])} -e NEXUS_PASSWORD=${nonsensitive(jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["NEXUS_PASSWORD"])}"
     ]
+  }
 }
-      }
