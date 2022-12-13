4175cbc779905c65ec7e4134ad4049b18f1b215f
Siny <sinydaniel93@gmail.com>
2022-12-12 21:10:17 -0500

    app deploy

--- terraform-mutable/app-deploy.tf
+++ terraform-mutable/app-deploy.tf
@@ -1,6 +1,9 @@
 resource "null_resource" "app-deploy" {
   count = length(aws_spot_instance_request.ec2-spot)
   provisioner "remote-exec" {
+    inline = [
+      "ansible-pull -U https://github.com/Siny93/ANSIBLE1.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${APP_VERSION} -e NEXUS_USERNAME=${local.NEXUS_USERNAME} -e NEXUS_PASSWORD=${local.NEXUS_PASSWORD}"
+    ]
     connection {
       type = "ssh"
       user = local.SSH_USERNAME
@@ -8,9 +11,6 @@ resource "null_resource" "app-deploy" {
       host = aws_spot_instance_request.ec2-spot.*.private_ip[count.index]
     }
 
-    inline = [
-      "ansible-pull -U https://github.com/Siny93/ANSIBLE1.git roboshop-pull.yml -e COMPONENT=${var.COMPONENT} -e ENV=${var.ENV} -e APP_VERSION=${APP_VERSION} -e NEXUS_USERNAME=${local.NEXUS_USERNAME} -e NEXUS_PASSWORD=${local.NEXUS_PASSWORD}"
-    ]
   }
 }
 
